a3a1f6d658508f03485fd79b1f926d6361f3b091
Ashukachrola <ashukachrola@gmail.com>
2024-10-16 13:14:51 +0530

    Flutter

---
+++ project_database/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+// import 'package:project_database/screens/homepage.dart';
+import 'package:sqflitecrud/screens/homepage.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
+}
